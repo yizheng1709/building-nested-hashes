a0cfe3e546d0262a0aeb3c3c867e8873fd17dff1
Eva <yi.zheng1709@gmail.com>
2021-01-14 18:49:19 -0500

    commit tree f5846bc19a794946d94bc66843e82884d44726ac

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -23,8 +23,9 @@ def fifth_challenge
    }
   }
 
-  #code your solution here:
-
+  epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   #Don't touch the following line! The method must return our newly modified epic tragedy hash
   epic_tragedy
